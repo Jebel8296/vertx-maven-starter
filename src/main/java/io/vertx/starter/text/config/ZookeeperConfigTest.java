0335ec39538a431050661af539f6d9b224c12bd5
xiaogao.xu@10020.cn <xiaogao.xu@10020.cn>
2017-10-20 16:36:29 +0800

    test vertx-config-zookeeper

--- src/main/java/io/vertx/starter/text/config/ZookeeperConfigTest.java
+++ src/main/java/io/vertx/starter/text/config/ZookeeperConfigTest.java
@@ -1,8 +1,5 @@
 package io.vertx.starter.text.config;
 
-import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.starter.common.GlobalVarialble;
 import io.vertx.starter.common.HuiAbstrackVerticle;
@@ -18,11 +15,7 @@ public class ZookeeperConfigTest extends HuiAbstrackVerticle {
       JsonObject jsonObject = (JsonObject) message.body();
       logger.info(jsonObject.encode());
 
-      ConfigStoreOptions storeOptions = new ConfigStoreOptions();
-      storeOptions.setType("zookeeper");
-      storeOptions.setConfig(new JsonObject().put("connection", "192.168.108.171:2181").put("path", "/" + jsonObject.getString("key")));
-      configRetriever = ConfigRetriever.create(vertx, new ConfigRetrieverOptions().addStore(storeOptions));
-      configRetriever.getConfig(handler -> {
+      getConfigFromZookeeper(jsonObject.getString("key")).setHandler(handler -> {
         if (handler.succeeded()) {
           JsonObject result = handler.result();
           message.reply(result);
